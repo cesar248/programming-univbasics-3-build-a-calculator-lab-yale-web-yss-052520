39101583bb00e62d4d8a8e521d36e618cb735592
Cesar Lara <cesar.lara@yale.edu>
2020-05-19 01:56:56 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -5,21 +5,31 @@ def addition(num1, num2)
 end
 
 def subtraction(num1, num2)
-
+  total = num1 - num2
+  puts total
+  return total 
 end
 
 def division(num1, num2)
-
+  total = num1 / num2 
+  puts total
+  return total
 end
 
 def multiplication(num1, num2)
-
+  total = num1 * num2
+  puts total
+  return total
 end
 
 def modulo(num1, num2)
-
+  total = num1 % num2
+  puts total 
+  return total
 end
 
 def square_root(num)
-
+  total = Math.sqrt(num)
+  puts total
+  return total
 end
